a7da3bd396955cdb5bcb56a1a1d25b80d9e4f181
brentjm <brent_maranzano@yahoo.com>
2019-10-27 16:30:56 -0400

    Working on React->Node-Red

--- nginx/crystallization-ui/src/process-map/process-map.js
+++ nginx/crystallization-ui/src/process-map/process-map.js
@@ -75,8 +75,9 @@ class ProcessMap extends Component {
   };
 
   componentDidMount = () => {
-    this.props.connectWebSocket("ws://10.131.0.175:1880/ws/crystallizer");
+    //this.props.connectWebSocket("ws://10.131.0.175:1880/ws/crystallizer");
     //this.props.connectWebSocket("ws://192.168.1.2:1880/ws/crystallizer");
+    this.props.connectWebSocket("ws://192.168.1.3:1880/ws/crystallizer");
     this.getElements(hardwareGroups, PVs);
     this.addEventListeners(hardwareGroups, PVs);
     this.formatSVG(hardwareGroups, PVs);
