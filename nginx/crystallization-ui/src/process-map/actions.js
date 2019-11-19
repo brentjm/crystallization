0cbea7ae917e92a90b202a2b35de607713b8a05c
brentjm <brent_maranzano@yahoo.com>
2019-11-19 16:42:27 -0500

    Added websocket send command

--- nginx/crystallization-ui/src/process-map/actions.js
+++ nginx/crystallization-ui/src/process-map/actions.js
@@ -1,3 +1,5 @@
+var socket = null;
+
 export const setProcessState = (processState) => ({
   type: "SET_PROCESS_STATE",
   processState
@@ -6,10 +8,15 @@ export const setProcessState = (processState) => ({
 export function connectWebSocket(ipAddress) {
   // Return a thunk that can be dispatched when the process-map is viewed
   return function(dispatch) {
-    const socket = new WebSocket(ipAddress);
+    socket = new WebSocket(ipAddress);
     socket.addEventListener('message', function (event) {
         let newProcessState = event.data;
         dispatch(setProcessState(newProcessState));
     });
   }
+};
+
+export function sendWebSocketCommand(command) {
+  command = JSON.stringify(command);
+  socket.send(command);
 };
