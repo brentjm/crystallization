949912c0bbc27d0b11fba218beb8c9618c4052f0
brentjm <brent_maranzano@yahoo.com>
2019-12-09 15:08:14 -0500

    Working on PostGres database

--- postgres/create_database.sql
+++ postgres/create_database.sql
@@ -4,6 +4,7 @@ create table infrared
     timestamp bigint primary key,
     username character varying(40),
     experiment character varying(40),
+    equipment character varying(40),
     absorption real[] not null
 );
 commit;
