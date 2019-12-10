802612b5c2e3d5e1a96c229a1c550a9a7cdc53da
brentjm <brent_maranzano@yahoo.com>
2019-12-10 18:12:48 -0500

    Finished working on everything

--- postgres/create_database.sql
+++ postgres/create_database.sql
@@ -2,9 +2,15 @@ begin;
 create table infrared
 (
     timestamp bigint primary key,
-    username character varying(40),
-    experiment character varying(40),
     equipment character varying(40),
+    command character varying(40),
     absorption real[] not null
+);
+create table fbrm
+(
+    timestamp bigint primary key,
+    equipment character varying(40),
+    command character varying(40),
+    histogram real[] not null
 );
 commit;
