3b28f775bd18578367343391f2621065b54e8554
brentjm <brent_maranzano@yahoo.com>
2020-08-25 10:06:22 -0400

    adding build files

---
+++ nginx/crystallization-ui/build/precache-manifest.f8da4056c704452bb54edfc014b44f00.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a40799343a679c392d3603ecd030656",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b13bf5d8f784131ad0a0",
+    "url": "/static/css/main.bc7e93a4.chunk.css"
+  },
+  {
+    "revision": "a4a3fdec1e85f11dec7c",
+    "url": "/static/js/2.2b572960.chunk.js"
+  },
+  {
+    "revision": "b13bf5d8f784131ad0a0",
+    "url": "/static/js/main.750d05f1.chunk.js"
+  },
+  {
+    "revision": "1ed7863968cb21a45dea",
+    "url": "/static/js/runtime-main.4d94b1a4.js"
+  },
+  {
+    "revision": "67ceff962bc6615b6e940a6c962efe96",
+    "url": "/static/media/crystallization_module.67ceff96.png"
+  },
+  {
+    "revision": "6dd68cd6f08ac0f3b92cebf538a1b669",
+    "url": "/static/media/grafana_dashboard.6dd68cd6.png"
+  },
+  {
+    "revision": "c2fc4779a555b3371f044be6c092928e",
+    "url": "/static/media/jupyter.c2fc4779.png"
+  },
+  {
+    "revision": "5e8ba0688e684f6f03f4e656a3d75665",
+    "url": "/static/media/nodered.5e8ba068.png"
+  },
+  {
+    "revision": "0d38ccb17d62a64b57aa7a844500b82e",
+    "url": "/static/media/process_map.0d38ccb1.png"
+  }
+]);
